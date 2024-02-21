b966763a9f80819d58bb3f4702abb3bb99e053d5
maharsh.s <maharsh.soni@pixelque.com>
2024-02-21 20:58:36 +0530

    all Setup configired

--- src/app.js
+++ src/app.js
@@ -17,4 +17,14 @@ app.use(express.json({ limit: '16Kb' }))
 app.use(express.urlencoded({ extended: true, limit: '16Kb' }))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//routes import 
+
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter)
+
 export { app }
